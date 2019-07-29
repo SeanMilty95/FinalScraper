92e428c6930b1ba40eb7e74f2bf3085e973ce864
SeanMilty95 <36428443+SeanMilty95@users.noreply.github.com>
2019-07-29 13:07:10 -0700

    download from moms

--- GUIhelp.py
+++ GUIhelp.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import WebDriverException
-from openpyxl import Workbook
 
 from MonthGUI import *
 from EditGUI import *
@@ -33,7 +31,6 @@ class Window(QMainWindow):
         self.ui.pushButton.clicked.connect(self.generate)
         self.ui.show_data.clicked.connect(self.show_data_page)
         self.ui.EditUnit.clicked.connect(self.edit_unit_info)
-        self.ui.ExcelDocument.clicked.connect(self.generate_excel)
 
     def hide_error_boxes(self):
         """Hides the Text Edit used to display error messages."""
@@ -75,63 +72,50 @@ class Window(QMainWindow):
         unit_name = self.ui.lineEdit.text()
         unit_url = self.ui.lineEdit_2.text()
 
-        try:
-            urllib.request.urlopen(unit_url)
-        except ValueError:
-            self.ui.URLErrorLabel.setText("Invalid URL! Check for https://")
-        except URLError:
-            self.ui.URLErrorLabel.setText("Invalid URL! Cannot be reached!")
+        if unit_name == '' or unit_url == '':
+            self.ui.ErrorEdit.show()
+            self.ui.ErrorEdit.setText('Please fill out the unit information in the spaces provided.')
         else:
-            self.ui.URLErrorLabel.setText("")
-            if unit_name == '' or unit_url == '':
-                self.ui.ErrorEdit.show()
-                self.ui.ErrorEdit.setText('Please fill out the unit information in the spaces provided.')
-            else:
-                self.ui.ErrorEdit.clear()
-                self.ui.ErrorEdit.hide()
-                valid_info = True
-            if valid_info:
-                if unit_name is not None and unit_url is not None:  # If a field is empty do not add to file
-                    self.ui.lineEdit.clear()
-                    self.ui.lineEdit_2.clear()
-                    date = str(datetime.date.today())
-                    rows = []
-                    new_dict = {'name': unit_name, 'url': unit_url, 'date': date}
-                    try:
-                        with open('units.txt', 'r+', newline='') as csvfile:
-                            reader = csv.DictReader(csvfile)
-                            for row in reader:
-                                rows.append(row)
-                            rows.append(new_dict)
-                    except IOError:
-                        print("Units file does not exist")
-                    
-                    with open('units.txt', 'w+', newline='') as csvfile:
-                        fieldnames = ['name', 'url', 'date']
-                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                        writer.writeheader()
-                        for i in range(len(rows)):
-                            writer.writerow(rows[i])
-
-                    # Maybe just add an append open and add to the end of file.
-
-                # Opens and reads from the list in units.txt
-                unit_list = []
+            self.ui.ErrorEdit.clear()
+            self.ui.ErrorEdit.hide()
+            valid_info = True
+        if valid_info:
+            if unit_name is not None and unit_url is not None:  # If a field is empty do not add to file
+                self.ui.lineEdit.clear()
+                self.ui.lineEdit_2.clear()
+                date = str(datetime.date.today())
+                rows = []
+                new_dict = {'name': unit_name, 'url': unit_url, 'date': date}
                 try:
                     with open('units.txt', 'r+', newline='') as csvfile:
                         reader = csv.DictReader(csvfile)
                         for row in reader:
-                            unit_list.append(row)
-                        self.all_lines = unit_list
+                            rows.append(row)
+                        rows.append(new_dict)
                 except IOError:
-                    print("No units.txt file")
+                    print("Units file does not exist")
+                    
+                with open('units.txt', 'w+', newline='') as csvfile:
+                    fieldnames = ['name', 'url', 'date']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for i in range(len(rows)):
+                        writer.writerow(rows[i])
 
-                layout = self.ui.scrollAreaWidgetContents.layout()
-                if layout is None or layout == '':
-                    self.fill_unit_list()
-                else:
-                    layout.addWidget(QCheckBox(unit_name))
-                    layout.update()
+                # Maybe just add an append open and add to the end of file.
+
+            # Opens and reads from the list in units.txt
+            in_file = open('units.txt', 'r')
+            unit_list = in_file.readlines()
+            self.all_lines = unit_list
+            in_file.close()
+
+            layout = self.ui.scrollAreaWidgetContents.layout()
+            if layout is None or layout == '':
+                self.fill_unit_list()
+            else:
+                layout.addWidget(QCheckBox(unit_name))
+                layout.update()
 
     def delete_unit(self):
         """Deletes a unit from the units.txt file and
@@ -143,6 +127,7 @@ class Window(QMainWindow):
         layout = self.ui.scrollAreaWidgetContents.layout()
         lines = []
         with open('units.txt', 'r') as f:
+            # lines = f.readlines()
             reader = csv.DictReader(f)
             for row in reader:
                 lines.append(row)
@@ -151,22 +136,14 @@ class Window(QMainWindow):
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
             for i in range(len(lines)):
+                # split_line = lines[i].split(' ')
                 if check_boxes[i].isChecked() is False:
+                    # f.write(lines[i])
                     writer.writerow(lines[i])
                 else:
                     check_boxes[i].setChecked(False)
                     check_boxes[i].hide()
                     layout.update()
-
-        unit_list = []
-        try:
-            with open('units.txt', 'r+', newline='') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    unit_list.append(row)
-                self.all_lines = unit_list
-        except IOError:
-            print("No units.txt file")
 
     def update_all(self):
         """Gathers and updates all the unit info for the units
@@ -195,8 +172,9 @@ class Window(QMainWindow):
                 # that they be cray
         self.get_info_for(units)
         # Generate the data GUI to present the data gathered from the listings html
+        current_month = datetime.datetime.now()
         for unit in units:
-            data_win = MonthWin(unit['name'])
+            data_win = DataWin(unit['name'], current_month.strftime("%B"))
             data_win.show()
             data_win.activateWindow()
 
@@ -206,12 +184,18 @@ class Window(QMainWindow):
         """
         self.ui.move(self.width + 500, self.height)
         check_boxes = self.ui.scrollAreaWidgetContents.findChildren(QCheckBox)
+        units = []
         for i in range(len(check_boxes)):
             if check_boxes[i].isChecked() is True:
+                #units.append(self.all_lines[i])
                 data_win = MonthWin(check_boxes[i].text())
                 data_win.show()
                 data_win.activateWindow()
                 check_boxes[i].setChecked(False)
+        #for unit in units:
+            #data_win = MonthWin(unit['name'])
+            #data_win.show()
+            #data_win.activateWindow()
 
     def get_info_for(self, units):
         """Starts the webdriver process and sends the needed information
@@ -223,27 +207,17 @@ class Window(QMainWindow):
         # Creates a driver for chrome
         opts = Options()
         opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (HTML, like Gecko) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/75.0.3770.100 Safari/537.36")
-        self.update_date(units)
         driver = webdriver.Chrome(chrome_options=opts)
-        count = 0
         for unit in units:
-            try:
-                driver.get(unit['url'])  # Goes to the url listed for the unit
-            except TypeError:
-                print("Invalid url! Type")
-            except WebDriverException:
-                print("Invalid url! WebDriver")
+            driver.get(unit['url'])  # Goes to the url listed for the unit
             # Right clicks and left clicks to inspect web page elements
-            if count == 0:
-                time.sleep(1)
-                actions = ActionChains(driver)
-                actions.context_click().perform()
-                time.sleep(0.5)
-                pyautogui.click(150, 356)
-                time.sleep(1)
-            count += 1
+            time.sleep(1)
+            actions = ActionChains(driver)
+            actions.context_click().perform()
+            pyautogui.click(150, 356)
+            time.sleep(1)
 
             # Generally the functions find the data you are looking for
             # Check the file HelperFunctions to specifically see what they do
@@ -257,65 +231,7 @@ class Window(QMainWindow):
         check_boxes = self.ui.scrollAreaWidgetContents.findChildren(QCheckBox)
         for box in check_boxes:
             if box.isChecked():
-                self.edit_win = EditWindow(box)
-                self.edit_win.show()
-                self.edit_win.activateWindow()
-                self.edit_win.ui.Accept.clicked.connect(self.edit_name)
-
-    def edit_name(self):
-        self.edit_win.update_data()
-        self.update_scroll_box()
-
-    def update_scroll_box(self):
-        name = self.edit_win.get_name()
-        old_name = self.edit_win.old_unit_name
-        check_boxes = self.ui.scrollAreaWidgetContents.findChildren(QCheckBox)
-        for box in check_boxes:
-            if box == self.edit_win.boxey:
-                box.setText(name)
-        self.edit_win.close()
-        try:
-            os.renames(old_name, name)
-        except FileNotFoundError:
-            print("May Not Have Changed Directory Name!\nPlease restart the program!")
-
-        unit_list = []
-        try:
-            with open('units.txt', 'r+', newline='') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    unit_list.append(row)
-                self.all_lines = unit_list
-        except IOError:
-            print("No units.txt file")
-
-    def update_date(self, units):
-        current_date = str(datetime.date.today())
-        remove = True
-        for unit in units:
-            with open('units.txt', 'r+', newline='') as inf, open('units_temp.txt', 'w', newline='') as outf:
-                reader = csv.DictReader(inf.readlines())
-                writer = csv.DictWriter(outf, fieldnames=['name', 'url', 'date'])
-                writer.writeheader()
-                for row in reader:
-                    try:
-                        if row['name'] == unit['name']:
-                            row['date'] = current_date
-                            writer.writerow(row)
-                        else:
-                            writer.writerow(row)
-                    except TypeError:
-                        print("Date could not update correctly")
-                        remove = False
-                writer.writerows(reader)
-            if remove is True:
-                os.remove('units.txt')
-                os.rename('units_temp.txt', 'units.txt')
-
-    def generate_excel(self):
-        print("Excel button pressed")
-        wb = Workbook()
-        ws = wb.active
-        ws.title = "Test Worksheet"
-        wb.save("test.xlsx")
-        os.startfile("test.xlsx")
+                edit_win = EditWindow(box.text())
+                edit_win.show()
+                edit_win.activateWindow()
+                box.setText(edit_win.new_name)
